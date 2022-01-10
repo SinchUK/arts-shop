eea5e9d41ad95586123a9e08e5b1c7d8ee254913
Gean <000cuiquesuum000@gmail.com>
2022-01-10 17:03:20 +0300

    Adder Calculator for Picture cost

--- src/js/modules/calc.js
+++ src/js/modules/calc.js
@@ -1,29 +1,101 @@
 const calc = (size, material, options, promocode, result) => {
-    const sizeBlock = document.querySelector(size),
-          materialBlock = document.querySelector(material),
-          optionsBlock = document.querySelector(options),
-          promocodeBlock = document.querySelector(promocode),
-          resultBlock = document.querySelector(result);
+  const sizeBlock = document.querySelector(size),
+    materialBlock = document.querySelector(material),
+    optionsBlock = document.querySelector(options),
+    promocodeBlock = document.querySelector(promocode),
+    resultBlock = document.querySelector(result);
 
-    let sum = 0;
+  let sum = 0;
 
-    const calcFunc = () => {
-        sum = Math.round((+sizeBlock.value) * (+materialBlock.value) + (+optionsBlock.value));
+  const calcFunc = () => {
+    sum = Math.round(
+      +sizeBlock.value * +materialBlock.value + (+optionsBlock.value)
+    );
 
-        if (sizeBlock.value == '' || materialBlock.value == '') {
-            resultBlock.textContent = "Пожалуйста выберите размер и материал картины";
+    if (sizeBlock.value == "" || materialBlock.value == "") {
+      resultBlock.textContent = "Пожалуйста выберите размер и материал картины";
+    } else if (promocodeBlock.value === "IWANTPOPART") {
+      resultBlock.textContent = Math.round(sum * 0.7);
+    } else {
+      resultBlock.textContent = sum;
+    }
+  };
 
-        } else if(promocodeBlock.value === 'IWANTPOPART') {
-            resultBlock.textContent = Math.round(sum * 0.7);
-        } else {
-            resultBlock.textContent = sum;
-        }
-    };
-
-    sizeBlock.addEventListener('change', calcFunc);
-    materialBlock.addEventListener('change', calcFunc);
-    optionsBlock.addEventListener('change', calcFunc);
-    promocodeBlock.addEventListener('input', calcFunc);
+  sizeBlock.addEventListener("change", calcFunc);
+  materialBlock.addEventListener("change", calcFunc);
+  optionsBlock.addEventListener("change", calcFunc);
+  promocodeBlock.addEventListener("input", calcFunc);
 };
-
 export default calc;
+
+// ______________________________________________________________
+
+// import { getResource } from "../services/requests";
+// const calc = (size, material, options, promocode, result) => {
+
+//     const sizeBlock = document.querySelector(size),
+//           materialBlock = document.querySelector(material),
+//           optionsBlock = document.querySelector(options),
+//           promocodeBlock = document.querySelector(promocode),
+//           resultBlock = document.querySelector(result);
+
+//         let sum = 0,
+//             sizeVal = '0',
+//             materialVal = '0',
+//             optionVal = '0';
+//         let state;
+
+//         getResource('assets/calcPrice.json')
+//             .then(res => {
+//                 state = res;
+//             })
+//             .catch(e => console.log(e));
+//         console.log(state, 'state');
+//         function changePrice(event, elem) {
+//             elem.addEventListener(event, (e) => {
+//                 const target = e.target,
+//                       select = target.id;
+
+//                 function calcFunc(state) {
+//                     for (let key in state[select]) {
+//                         if (elem.value === key) {
+//                             switch(select) {
+//                                 case 'size':
+//                                     sizeVal = state[select][key];
+//                                     break;
+//                                 case 'material':
+//                                     materialVal = state[select][key];
+//                                     break;
+//                                 case 'options':
+//                                     optionVal = state[select][key];
+//                                     break;
+//                             }
+//                         }
+//                         console.log(state[select][key]);
+//                     }
+
+//                     sum = Math.round((+sizeBlock.value) * (+materialBlock.value) + (+optionsBlock.value));
+//                     if (sizeBlock.value == '' || materialBlock.value == '') {
+//                         resultBlock.textContent = "Пожалуйста выберите размер и материал картины";
+
+//                     } else if(promocodeBlock.value === 'IWANTPOPART') {
+//                         resultBlock.textContent = Math.round(sum * 0.7);
+//                     } else {
+//                         resultBlock.textContent = sum;
+//                     }
+
+//                     console.log(resultBlock.textContent, 'resbl txtctt');
+//                 }
+
+//                 calcFunc(state);
+//             });
+//         }
+
+//     changePrice('change', sizeBlock);
+//     changePrice('change', materialBlock);
+//     changePrice('change', optionsBlock);
+//     changePrice('input', promocodeBlock);
+
+// };
+
+// export default calc;
--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -2,117 +2,110 @@
 import { postData } from "../services/requests";
 
 const forms = () => {
-    const form = document.querySelectorAll('form'),
-          inputs = document.querySelectorAll('input'),
-          upload = document.querySelectorAll('[name = "upload"]'),
-          textAreas = document.querySelectorAll('[textarea]');
-          
-    // checkNumInputs('input[name="user_phone"]');
-    
-    const message = {
-        loading: 'Загрузка...',
-        success: 'Спасибо! Скоро мы с вами свяжемся',
-        failure: 'Что-то пошло не так...',
-        spinner: 'assets/img/spinner.gif',
-        ok: 'assets/img/ok.png',
-        fail: 'assets/img/fail.png'
-    };
+  const form = document.querySelectorAll("form"),
+    inputs = document.querySelectorAll("input"),
+    upload = document.querySelectorAll('[name = "upload"]'),
+    textAreas = document.querySelectorAll("[textarea]");
 
-    const path = {
-        designer: 'assets/server.php',
-        question: 'assets/question.php'
-    };
+  // checkNumInputs('input[name="user_phone"]');
 
-   
+  const message = {
+    loading: "Загрузка...",
+    success: "Спасибо! Скоро мы с вами свяжемся",
+    failure: "Что-то пошло не так...",
+    spinner: "assets/img/spinner.gif",
+    ok: "assets/img/ok.png",
+    fail: "assets/img/fail.png",
+  };
 
-    const clearInputs = () => {
+  const path = {
+    designer: "assets/server.php",
+    question: "assets/question.php",
+  };
 
-        inputs.forEach(item => {
+  const clearInputs = () => {
+    inputs.forEach((item) => {
+      item.value = "";
+    });
 
-            item.value = '';
-        });
+    textAreas.forEach((item) => {
+      item.value = "";
+    });
 
-        textAreas.forEach(item => {
-            item.value = '';
-        });
-        
-        upload.forEach(item => {
-            item.previousElementSibling.textContent = "Файл не выбран";
-        });
-    };
+    upload.forEach((item) => {
+      item.previousElementSibling.textContent = "Файл не выбран";
+    });
+  };
 
-    upload.forEach(item => {
-        item.addEventListener('input', ()=> {
-            console.log(item.files[0]);
-            let dots;
-            const arr = item.files[0].name.split('.');
-            arr[0].length > 6 ? dots = '...' : dots = '.';
-            const name = arr[0].substring(0, 6) + dots + arr[1];
-            item.previousElementSibling.textContent = name;
+  upload.forEach((item) => {
+    item.addEventListener("input", () => {
+      // console.log(item.files[0]);
+      let dots;
+      const arr = item.files[0].name.split(".");
+
+      arr[0].length > 6 ? (dots = "...") : (dots = ".");
+      const name = arr[0].substring(0, 6) + dots + arr[1];
+      item.previousElementSibling.textContent = name;
+    });
+  });
+
+  form.forEach((item) => {
+    item.addEventListener("submit", (e) => {
+      e.preventDefault();
+
+      let statusMessage = document.createElement("div");
+      statusMessage.classList.add("status");
+      item.parentNode.appendChild(statusMessage);
+
+      item.classList.add("animated", "fadeOutUp");
+      setTimeout(() => {
+        item.style.display = "none";
+      }, 400);
+
+      let statusImg = document.createElement("img");
+      statusImg.setAttribute("src", message.spinner);
+      statusImg.classList.add("animated", "fadeInUp");
+      statusMessage.appendChild(statusImg);
+
+      let textMessage = document.createElement("div");
+      textMessage.textContent = message.loading;
+      statusMessage.appendChild(textMessage);
+
+      const formData = new FormData(item);
+
+      // const totalPrice = +document.querySelector('.calc-price').innerHTML;
+
+      // if (item.classList.contains('calc_form')) {
+      //     formData.append('total', totalPrice );
+      // }
+
+      // const json = JSON.stringify(Object.fromEntries(formData));
+      // console.log(json, 'json');
+
+      let api;
+      item.closest(".popup-design") || item.classList.contains("calc_form") ? (api = path.designer) : (api = path.question);
+
+      postData(api, formData)
+        .then((res) => {
+          console.log(res, "res");
+          statusImg.setAttribute("src", message.ok);
+          textMessage.textContent = message.success;
+        })
+        .catch(() => {
+          statusImg.setAttribute("src", message.fail);
+          textMessage.textContent = message.failure;
+        })
+        .finally(() => {
+          clearInputs();
+          setTimeout(() => {
+            statusMessage.remove();
+            item.style.display = "block";
+            item.classList.remove("fadeOutUp");
+            item.classList.add("fadeInUp");
+          }, 5000);
         });
     });
-        
-
-    form.forEach(item => {
-        item.addEventListener('submit', (e) => {
-            e.preventDefault();
-
-            let statusMessage = document.createElement('div');
-            statusMessage.classList.add('status');
-            item.parentNode.appendChild(statusMessage);
-
-            item.classList.add('animated', 'fadeOutUp');
-            setTimeout(()=> {
-                item.style.display = 'none';
-            }, 400);
-
-            let statusImg = document.createElement('img');
-            statusImg.setAttribute('src', message.spinner); 
-            statusImg.classList.add('animated', 'fadeInUp');
-            statusMessage.appendChild(statusImg);
-
-            let textMessage = document.createElement('div');
-            textMessage.textContent = message.loading;
-            statusMessage.appendChild(textMessage);
-
-            const formData = new FormData(item);
-
-            const totalPrice = document.querySelector('.calc-price').innerHTML;
-            console.log(totalPrice, 'totalPrice');
-            const total = {
-                total: totalPrice
-            };
-
-            if (item.classList.contains('calc-form')) {
-                formData.append(total);
-                console.log(formData, 'formData');
-            }
-            
-            let api;
-            item.closest('.popup-design') || item.classList.contains('calc_form') ?
-            api = path.designer : api = path.question;
-
-            postData(api, formData)
-                .then(res => {
-                    console.log(res, 'res');
-                    statusImg.setAttribute('src', message.ok);
-                    textMessage.textContent = message.success;
-                })
-                .catch(() => {
-                    statusImg.setAttribute('src', message.fail);
-                    textMessage.textContent = message.failure;
-                })
-                .finally(() => {
-                    clearInputs();
-                    setTimeout(() => {
-                        statusMessage.remove();
-                        item.style.display = 'block';
-                        item.classList.remove('fadeOutUp');
-                        item.classList.add('fadeInUp');
-                    }, 5000);
-                });
-        });
-    });
+  });
 };
 
 export default forms;
